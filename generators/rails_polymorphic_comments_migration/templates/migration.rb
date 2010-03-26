5c2af68c4900ee9d955e0df4ed65d117f1b6cdc2
root <root@sandip-laptop.(none)>
2010-03-26 16:19:02 +0530

    removed unnecessary code

--- generators/rails_polymorphic_comments_migration/templates/migration.rb
+++ generators/rails_polymorphic_comments_migration/templates/migration.rb
@@ -1,24 +1,16 @@
 class CreatePolymorphicComments < ActiveRecord::Migration
   def self.up
     create_table :comments do |t|
-      
       # POLYMORPHIC COMMENTS
-      
       # resource type can post, site, review, idea, etc
       t.references :resource, :polymorphic => true
       t.string :body
       t.references :user
-      
+
       t.timestamps
     end
   end
-
-  def self.down
-    drop_table :comments
-  end
-
-  end
-
+  
   def self.down
     drop_table :comments
   end
